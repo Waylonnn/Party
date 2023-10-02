2f70de384b24f1a4ef2d62dde81c24521bce9037
Waylon <weese.619547@swl.k12.oh.us>
2023-10-02 09:27:34 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+    /*Write a program that prompts the user for their party affiliation (Democrat, Republican, or Independent) and
+    responds accordingly with a Donkey, Elephant, or Person  (i.e. “You get a Democratic Donkey.”).D
+
+    Note: Create a menu so the user chooses D, R, or I and assume that any other choice will be Other.
+    Use a cascaded if structure (if, else if, else), not multiple standalone if statements.
+    Tests Runs:
+    D, R, I, and Other as user input*/
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String party;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your party affiliation? (D, R, or I): ");
+        party = scan.nextLine().toUpperCase();
+        if(party.equals("D")){
+            System.out.println("You get a Democratic Donkey!");
+        } else if(party.equals("R")){
+            System.out.println("You get a Republican Elephant!");
+        } else if(party.equals("I")){
+            System.out.println("You get a Independent Person!");
+        } else{
+            System.out.println("Error: Invalid Party.");
+        }
     }
 }
